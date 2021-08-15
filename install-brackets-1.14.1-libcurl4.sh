7a9e754b26efe29772de6e600d9f8f07332d2073
Andi Hafner <andihafner@andihafner.com>
2021-08-15 13:16:14 +0200

    bash skeleton implemented

--- install-brackets-1.14.1-libcurl4.sh
+++ install-brackets-1.14.1-libcurl4.sh
@@ -1,38 +1,67 @@
 #!/bin/bash
-: "
-brackets-libcurl4 v 1.0
 
-This script installs the latest Linux-Version of Adobe Brackets on
-newer Debian based Linux Distros
-Adobes support for Brackets ends on September 2021
+show_info() {                                                                   
+echo -e "                                                                       
+install-brackets-1.14.1-libcurl4 by kmu-net.ch / andihafner.com 2021 Version 0.1
+This work is licensed under the Apache License Version 2.0
 
-Copyright andihafner.com 2021, Apache License Version 2.0
-after instructions from
+This script installs the Linux-Version 1.14.1 of Adobe Brackets on newer Debian
+based Linux Distros which require the libcurl4 library instead of libcurl3.
+
+It repacks the original Package as described here:
 https://askubuntu.com/questions/1238601/brackets-no-extensions-available
+
 "
+}        
+
+#-------------------------------------------------------------------------------
+
 : "
-# Establish temporary directory
-mkdir --parents ~/temp/brackets-libcurl4
-cd ~/temp/brackets-libcurl4
+Todo:
 "
 
-#: "
-# Get OS-Architecture
-if [ $(uname --hardware-platform | grep --count x86_64) == "1" ]
-  then
-    os_arch="64"
-  else
-    os_arch="32"
-fi
-#"
+#-------------------------------------------------------------------------------
 
-#: "
-# Downloading Debian Package
-wget https://github.com/adobe/brackets/releases/download/release-1.14.1/Brackets.Release.1.14.1.$os_arch-bit.deb
-#"
+ask_for_continuation() {
 
-#: "
-#os_arch="64"
-#echo $os_arch
-#"
+  echo -e -n "Would you like to continue? (Type [y/n] followed by ENTER: "
+  read answer
+
+}
+#-------------------------------------------------------------------------------
+
+get_os_arch() {
+
+  echo "The answer was yes"
+
+}
+
+#-------------------------------------------------------------------------------
+
+convert_hms2seconds() {
+    seconds="$(echo $hms | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')"
+}
+
+#-------------------------------------------------------------------------------
+
+main() {
+	show_info
+  ask_for_continuation
+  if [ $answer == "n" ]
+    then
+      echo -e "\nGood Bye...\n"
+    else
+      get_os_arch
+  fi
+}
+
+#-------------------------------------------------------------------------------
+
+case "$1" in
+
+#	""|"--help"|"-h")    show_help;;
+
+  *)                   main
+
+esac
 
